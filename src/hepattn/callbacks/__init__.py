414a7670c781bd1d074966f973e15b2c501106f0
Sam Van Stroud <sam.van.stroud@cern.ch>
2025-03-13 15:27:37 +0000

    fix imports

--- src/hepattn/callbacks/__init__.py
+++ src/hepattn/callbacks/__init__.py
@@ -1,4 +1,5 @@
 from hepattn.callbacks.compile import Compile
+from hepattn.callbacks.inference_timer import InferenceTimer
 from hepattn.callbacks.throughput_monitor import MyThroughputMonitor
 
-__all__ = ["Compile", "MyThroughputMonitor"]
+__all__ = ["Compile", "InferenceTimer", "MyThroughputMonitor"]
