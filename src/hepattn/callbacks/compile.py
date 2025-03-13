e7ca2ac5b3715370a3ea95c326965e43d32ece81
Sam Van Stroud <sam.van.stroud@cern.ch>
2025-03-13 15:25:44 +0000

    improved compile callback

--- src/hepattn/callbacks/compile.py
+++ src/hepattn/callbacks/compile.py
@@ -1,17 +1,14 @@
-import torch
 from lightning import Callback
 
 
 class Compile(Callback):
-    def on_train_start(self, trainer, pl_module):  # noqa: ARG002
+    def setup(self, trainer, pl_module, stage):  # noqa: ARG002
         self.compile(pl_module)
 
-    def on_test_start(self, trainer, pl_module):  # noqa: ARG002
-        self.compile(pl_module)
-
-    def compile(self, pl_module):
-        print("compiling encoder...")
-        if hasattr(pl_module, "encoder"):
-            pl_module.encoder = torch.compile(pl_module.encoder, dynamic=True)
-        elif hasattr(pl_module.model, "encoder"):
-            pl_module.model.encoder = torch.compile(pl_module.model.encoder, dynamic=True)
+    def compile(self, module):
+        print("-" * 80)
+        print("compiling model...")
+        for name, submodule in module.named_children():
+            print(f" -> compiling {name}...")
+            submodule.compile(dynamic=True)
+        print("-" * 80, "\n")
