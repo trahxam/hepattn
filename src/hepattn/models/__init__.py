e741e3c2ed85412de13054b594fccd388f2bff13
Max Hart <maxhart@compute-gpu-0-5.local>
2025-04-01 14:20:10 +0100

    refactor cleanup boilerplate

--- src/hepattn/models/__init__.py
+++ src/hepattn/models/__init__.py
@@ -3,11 +3,15 @@ from hepattn.models.attention import Attention
 from hepattn.models.dense import Dense
 from hepattn.models.norm import LayerNorm, RMSNorm
 from hepattn.models.posenc import PositionEncoder
+from hepattn.models.input import InputNet
 from hepattn.models.transformer import DropPath, Encoder, EncoderLayer, LayerScale, Residual
+from hepattn.models.decoder import MaskFormerDecoderLayer
+from hepattn.models.maskformer import MaskFormer
 
 __all__ = [
     "Attention",
     "Dense",
+    "InputNet",
     "DropPath",
     "Encoder",
     "EncoderLayer",
@@ -17,4 +21,6 @@ __all__ = [
     "RMSNorm",
     "Residual",
     "SwiGLU",
+    "MaskFormerDecoderLayer",
+    "MaskFormer",
 ]
