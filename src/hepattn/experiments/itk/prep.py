fix bug where int overflow messed up masks for itk tracking

src/hepattn/experiments/itk/prep.py
@@ -2,6 +2,7 @@ from argparse import ArgumentParser
 from pathlib import Path
 
 import pandas as pd
+import numpy as np
 
 
 # A script for preprocessing ITk CSV files into parquet binary files
@@ -36,11 +37,11 @@ def preprocess(in_dir: str, out_dir: str, overwrite: bool):
             continue
 
         # Load the particles and hits information
-        parts = pd.read_csv(parts_in_path)
-        truth = pd.read_csv(truth_in_path)
+        parts = pd.read_csv(parts_in_path, dtype={"particle_id": np.int64})
+        truth = pd.read_csv(truth_in_path, dtype={"tgt_id": np.int64})
 
         # Rename hit fields to be compatible with TrackML
-        truth.rename(columns={"tgt_pid": "particle_id", "volume_id": "region"}, inplace=True)
+        truth.rename(columns={"tgt_pid": "particle_id"}, inplace=True)
         
         # Rename particle fields to be compatible with TrackML
         parts.rename(columns={"q": "charge", "particle_type": "pdgId", "nhits": "num_clusters"}, inplace=True)