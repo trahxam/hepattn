13859bcb8b94b9d3d0254e97945e3fedef88357b
Max Hart <maxhart@compute-gpu-0-5.local>
2025-04-05 15:59:20 +0100

    itk dataset improvements

--- tests/models/test_matcher.py
+++ tests/models/test_matcher.py
@@ -18,27 +18,22 @@ class TestMatcher:
         pred_mask = torch.clone(true_mask[:,torch.randperm(NUM_QUERIES),:])
         return pred_mask, true_mask
     
-    @pytest.fixture
-    def scipy_matcher(self):
-        matcher = Matcher(
-            default_solver="scipy",
-            adaptive_solver=False,
-        )
-        return matcher
-    
-    def test_matcher(self, mock_masks, scipy_matcher):
+    def test_matcher(self, mock_masks):
         pred_mask, true_mask = mock_masks
 
         costs_ce = mask_ce_costs(pred_mask, true_mask)
         costs_dice =  mask_dice_costs(pred_mask, true_mask)
         costs_focal = mask_focal_costs(pred_mask, true_mask)
 
-        for costs in [costs_ce, costs_dice, costs_focal]:
-            pred_idxs = scipy_matcher(costs)
+        for solver in ["scipy"]:
+            matcher = Matcher(default_solver=solver, adaptive_solver=False)
+            
+            for costs in [costs_ce, costs_dice, costs_focal]:
+                pred_idxs = matcher(costs)
 
-            batch_idxs = torch.arange(costs.shape[0]).unsqueeze(1).expand(-1, costs.shape[-1])
+                batch_idxs = torch.arange(costs.shape[0]).unsqueeze(1).expand(-1, costs.shape[-1])
 
-            pred_mask_matched = pred_mask[batch_idxs,pred_idxs]
+                pred_mask_matched = pred_mask[batch_idxs,pred_idxs]
 
-            # We should be able to exactly recover the original mask
-            assert torch.all(true_mask == pred_mask_matched)
+                # We should be able to exactly recover the original mask
+                assert torch.all(true_mask == pred_mask_matched)
