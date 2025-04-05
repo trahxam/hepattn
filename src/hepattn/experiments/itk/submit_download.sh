6ffdc851fbd86066a9cabd86f47ef61dfe44169c
Max Hart <maxhart@hypatia-login.local>
2025-04-05 07:21:41 +0100

    initial itk support

---
+++ src/hepattn/experiments/itk/submit_download.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#SBATCH --job-name=itk-download
+#SBATCH -p RCIF
+#SBATCH --export=ALL
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=24G
+#SBATCH --output=/share/rcifdata/maxhart/hepattn-test/hepattn/src/hepattn/experiments/itk/slurm_logs/slurm-%j.%x.out
+
+# Used for downloading raw ITk samples via rucio
+
+# Move to workdir
+cd /share/rcifdata/maxhart/hepattn-test/hepattn/
+echo "Moved dir, now in: ${PWD}"
+
+# Set tmpdir
+export TMPDIR=/var/tmp/
+
+# Run the preprocessing
+echo "Running preprocessing script..."
+
+# Where the data will be downloaded to
+INPUT_DIR="/share/rcifdata/maxhart/data/itk/"
+
+cd $INPUT_DIR
+echo "Moved dir, now in: ${PWD}"
+
+# Setup atlas boilerplate
+source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
+lsetup rucio
+
+# Setup ssh key so we can activate the voms proxy
+eval $(ssh-agent -s)
+ssh-add /home/maxhart/.globus/userkey.pem
+
+# Activate the proxy
+voms-proxy-init -voms atlas
+
+# Start the download
+rucio download user.avallier:ATLAS-P2-RUN4-03-00-00_Rel.24_ttbar_uncorr_pu200_dumpv5_acorn2.0.0_acorn_data_reading_output_trainset
+#rucio download user.avallier:ATLAS-P2-RUN4-03-00-00_Rel.24_ttbar_uncorr_pu200_dumpv5_acorn2.0.0_acorn_data_reading_output_valset
+#rucio download user.avallier:ATLAS-P2-RUN4-03-00-00_Rel.24_ttbar_uncorr_pu200_dumpv5_acorn2.0.0_acorn_data_reading_output_testset
+
+ssh-agent -k
+echo "Done!"
---
+++ src/hepattn/experiments/itk/submit_prep_rcif.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#SBATCH --job-name=itk-prep
+#SBATCH -p RCIF
+#SBATCH --export=ALL
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=24G
+#SBATCH --output=/share/rcifdata/maxhart/hepattn-test/hepattn/src/hepattn/experiments/itk/slurm_logs/slurm-%j.%x.out
+
+# Used for preprocessing raw ITk samples into binary parquet files used for training
+
+# Move to workdir
+cd /share/rcifdata/maxhart/hepattn-test/hepattn/
+echo "Moved dir, now in: ${PWD}"
+
+# Set tmpdir
+export TMPDIR=/var/tmp/
+
+# Run the preprocessing
+echo "Running preprocessing script..."
+
+# Change these to wherever your data is, or get access to them
+IN_DIR="/share/rcifdata/maxhart/data/itk/ATLAS-P2-RUN4-03-00-00_Rel.24_ttbar_uncorr_pu200_dumpv5_acorn2.0.0_acorn_data_reading_output_trainset/"
+OUT_DIR="/share/rcifdata/maxhart/data/itk/train/"
+
+# Python command that will be run
+PYTORCH_CMD="python src/hepattn/experiments/itk/prep.py --in_dir $IN_DIR --out_dir $OUT_DIR"
+
+# Pixi commnand that runs the python command inside the pixi env
+PIXI_CMD="pixi run $PYTORCH_CMD"
+
+# Apptainer command that runs the pixi command inside the pixi apptainer image
+APPTAINER_CMD="apptainer run --bind /share/rcifdata/maxhart /share/rcifdata/maxhart/hepattn-test/hepattn/pixi.sif $PIXI_CMD"
+
+# Run the final command
+echo "Running command: $APPTAINER_CMD"
+$APPTAINER_CMD
+echo "Done!"
