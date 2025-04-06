459a7527429b6cc022664a4511b7f759c6d0d711
Max Hart <maxhart@compute-gpu-0-5.local>
2025-04-06 13:23:33 +0100

    implement working cld dataloader

--- src/hepattn/experiments/cld/data.py
+++ src/hepattn/experiments/cld/data.py
@@ -1,6 +1,7 @@
 import numpy as np
 from pathlib import Path
 import torch
+import awkward as ak
 import time
 from torch.utils.data import DataLoader, Dataset
 
@@ -10,25 +11,25 @@ class CLDDataset(Dataset):
         self,
         dirpath: str,
         inputs: dict,
-        labels: dict,
+        targets: dict,
         num_events: int = -1,
-        num_particles: int = 250,
+        event_max_num_particles: int = 1000,
         random_seed: int = 42,
     ):
         super().__init__()
 
         self.dirpath = dirpath
         self.inputs = inputs
-        self.labels = labels
+        self.targets = targets
         self.num_events = num_events
-        self.num_particles = num_particles
+        self.event_max_num_particles = event_max_num_particles
         self.random_seed = random_seed
 
         # Global random state initialisation
         np.random.seed(random_seed)
 
         # Setup the number of events that will be used
-        event_filenames = list(Path(self.dirpath).glob("*event*.npy"))
+        event_filenames = list(Path(self.dirpath).glob("*reco*.parquet"))
         num_available_events = len(event_filenames)
         num_requested_events = num_available_events if num_events == -1 else num_events
         self.num_events = min(num_available_events, num_requested_events)
@@ -42,82 +43,92 @@ class CLDDataset(Dataset):
         return int(self.num_events)
     
     def __getitem__(self, idx):
-        event = np.load(self.event_filenames[idx], allow_pickle=True)[()]
+        event = self.load_event(idx)
+
+        # TODO: Clean this up, will need to be changed to support hit-particle regression
+
+        inputs = {}
+        input_sizes = {}
+        for input_name, fields in self.inputs.items():
+            field_sizes = set()
+            for field in fields:
+                x = ak.to_numpy(event[f"{input_name}.{field}"])[0]
+                assert np.all(~np.isnan(x))
+                field_size = len(x)
+                inputs[f"{input_name}_{field}"] = torch.from_numpy(x).unsqueeze(0).half()
+                field_sizes.add(field_size)
+            
+            assert len(field_sizes) == 1, f"Found mismatching field sizes for {input_name}"
+            input_sizes[input_name] = next(iter(field_sizes))
+
+            inputs[f"{input_name}_valid"] = torch.ones((1, input_sizes[input_name]), dtype=torch.bool)
+
+        targets = {}
+        target_sizes = {}
+        for target_name, fields in self.targets.items():
+
+            field_sizes = set()
+            for field in fields:
+                x = ak.to_numpy(event[f"{target_name}.{field}"])[0]
+                field_size = len(x)
+                assert np.all(~np.isnan(x))
+                x_padding = np.full(self.event_max_num_particles - field_size, np.nan)
+                x = np.concatenate((x, x_padding), axis=-1)
+                targets[f"{target_name}_{field}"] = torch.from_numpy(x).unsqueeze(0).half()
+                field_sizes.add(field_size)
+            
+            assert len(field_sizes) == 1, f"Found mismatching field sizes for {target_name}"
+            target_sizes[target_name] = next(iter(field_sizes))
+
+            targets[f"{target_name}_valid"] = torch.zeros((1, self.event_max_num_particles), dtype=torch.bool)
+            targets[f"{target_name}_valid"][:,:target_sizes[target_name]] = True
+
+        # Create the masks that link particles to hits
+        for input_name in self.inputs.keys():
+            num_hits = input_sizes[input_name]
+            mask = np.full((num_hits, self.event_max_num_particles), False)
+            # Get the mask indices that map from hits to particles
+            mask_idxs = ak.to_numpy(event[f"{input_name}_to_particle_idxs"])[0]
+            mask[mask_idxs[:,0],mask_idxs[:,1]] = True
+            # Have to transpose the mask to get mask for particles to hits
+            targets[f"particle_{input_name}_mask"] = torch.from_numpy(mask.T).unsqueeze(0).bool()
+        
+        return inputs, targets
+
+    def load_event(self, idx):
+        event = ak.from_parquet(self.event_filenames[idx])
 
         def convert_mm_to_m(i, p):
             # Convert a spatial coordinate from mm to m inplace
             for coord in ["x", "y", "z"]:
-                event[f"{i}_{p}.{coord}"] = 0.001 * event[f"{i}_{p}.{coord}"]
+                event[f"{i}.{p}.{coord}"] = 0.001 * event[f"{i}.{p}.{coord}"]
 
         def add_cylindrical_coords(i, p):
             # Add standard tracking cylindrical coordinates
-            event[f"{i}_{p}.r"] = np.sqrt(event[f"{i}_{p}.x"]**2 + event[f"{i}_{p}.y"]**2)
-            event[f"{i}_{p}.s"] = np.sqrt(event[f"{i}_{p}.x"]**2 + event[f"{i}_{p}.y"]**2 + event[f"{i}_{p}.z"]**2)
-            event[f"{i}_{p}.theta"] = np.arccos(event[f"{i}_{p}.z"] / event[f"{i}_{p}.s"])
-            event[f"{i}_{p}.eta"] = np.arctanh(event[f"{i}_{p}.z"] / event[f"{i}_{p}.s"])
-            event[f"{i}_{p}.phi"] = np.arctan2(event[f"{i}_{p}.y"], event[f"{i}_{p}.x"])
+            event[f"{i}.{p}.r"] = np.sqrt(event[f"{i}.{p}.x"]**2 + event[f"{i}.{p}.y"]**2)
+            event[f"{i}.{p}.s"] = np.sqrt(event[f"{i}.{p}.x"]**2 + event[f"{i}.{p}.y"]**2 + event[f"{i}.{p}.z"]**2)
+            event[f"{i}.{p}.theta"] = np.arccos(event[f"{i}.{p}.z"] / event[f"{i}.{p}.s"])
+            event[f"{i}.{p}.eta"] = np.arctanh(event[f"{i}.{p}.z"] / event[f"{i}.{p}.s"])
+            event[f"{i}.{p}.phi"] = np.arctan2(event[f"{i}.{p}.y"], event[f"{i}.{p}.x"])
         
         def add_conformal_coords(i, p):
             # Conformal tracking coordinates
             # https://indico.cern.ch/event/658267/papers/2813728/files/8362-Leogrande.pdf
-            event[f"{i}_{p}.u"] = event[f"{i}_{p}.x"] / (event[f"{i}_{p}.x"]**2 + event[f"{i}_{p}.y"]**2)
-            event[f"{i}_{p}.v"] = event[f"{i}_{p}.y"] / (event[f"{i}_{p}.x"]**2 + event[f"{i}_{p}.y"]**2)
+            event[f"{i}.{p}.u"] = event[f"{i}.{p}.x"] / (event[f"{i}.{p}.x"]**2 + event[f"{i}.{p}.y"]**2)
+            event[f"{i}.{p}.v"] = event[f"{i}.{p}.y"] / (event[f"{i}.{p}.x"]**2 + event[f"{i}.{p}.y"]**2)
 
         # Create the input hit objects - only fields that are specified in the config are sent
-        inputs = {}
         for input_name, fields in self.inputs.items():
-            add_cylindrical_coords(input_name, "position")
-            add_conformal_coords(input_name, "position")
-            convert_mm_to_m(input_name, "position")
-
-            for field in fields:
-                inputs[f"{input_name}_{field}"] = torch.from_numpy(event[f"{input_name}_{field}"])
+            add_cylindrical_coords(input_name, "pos")
+            add_conformal_coords(input_name, "pos")
+            convert_mm_to_m(input_name, "pos")
 
         # Create the label particle objects
-        labels = {}
-        for label_name, fields in self.labels.items():
-            add_cylindrical_coords(label_name, "vertex")
-            add_conformal_coords(label_name, "vertex")
-            convert_mm_to_m(label_name, "vertex")
+        for target_name, fields in self.targets.items():
+            for point in ["vtx", "end"]:
+                add_cylindrical_coords(target_name, f"{point}.pos")
+                add_conformal_coords(target_name, f"{point}.pos")
+                convert_mm_to_m(target_name, f"{point}.pos")
+                add_cylindrical_coords(target_name, f"{point}.mom")
 
-            add_cylindrical_coords(label_name, "endpoint")
-            add_conformal_coords(label_name, "endpoint")
-            convert_mm_to_m(label_name, "endpoint")
-
-            add_cylindrical_coords(label_name, "momentum")
-            add_cylindrical_coords(label_name, "momentumAtEndpoint")
-
-            for field in fields:
-                labels[f"{label_name}_{field}"] = torch.from_numpy(event[f"{label_name}_{field}"])
-
-        # Create the masks that link particles to hits
-        masks = {}
-        for input_name in self.inputs.keys():
-            mask = np.full((event[f"{input_name}_position.x"].shape[0], self.num_particles), False)
-            # Get the mask indices that map from hits to particles
-            mask_idxs = event[f"{input_name}_to_particle_idxs"]
-            mask[mask_idxs[:,0],mask_idxs[:,1]] = True
-            # Have to transpose the mask to get mask for particles to hits
-            masks[f"particle_{input_name}"] = mask.T
-        
-        
-
-        return inputs, labels, masks
-
-
-
-
-import yaml
-
-config = yaml.safe_load(Path("config.yaml").read_text())
-
-dataset = CLDDataset(
-    dirpath="test_data_out",
-    inputs=config["data"]["inputs"],
-    labels=config["data"]["labels"],
-    num_events=50,
-    num_particles=250,
-)
-
-
-dataset[0]
+        return event
---
+++ src/hepattn/experiments/cld/plot_event.py
@@ -0,0 +1,48 @@
+import matplotlib.pyplot as plt
+
+
+def plot_cld_event_reconstruction(inputs, reconstruction, axes_spec):
+    num_axes = len(axes_spec)
+
+    fig, ax = plt.subplots(1, num_axes)
+    fig.set_size_inches(8 * num_axes, 8)
+    
+    if num_axes == 1:
+        ax = [ax]
+    else:
+        ax = ax.flatten()
+
+    colormap = plt.cm.tab10
+    cycler = [colormap(i) for i in range(colormap.N)]
+
+    batch_idx = 0
+    num_particles = reconstruction["particle_valid"].sum()
+
+    for ax_idx, ax_spec in enumerate(axes_spec):
+        for input_name in ax_spec["input_names"]:
+            x = inputs[f"{input_name}_{ax_spec['x']}"][batch_idx]
+            y = inputs[f"{input_name}_{ax_spec['y']}"][batch_idx]
+                       
+            ax[ax_idx].scatter(x, y, s=1.0, color="black")
+            
+            for mcparticle_idx in range(num_particles):
+                color = cycler[mcparticle_idx % len(cycler)]
+                mask = reconstruction[f"particle_{input_name}_mask"][batch_idx][mcparticle_idx]
+
+                # Tracker hit
+                if input_name[1] == "t":
+                    ax[ax_idx].plot(x[mask], y[mask], color=color, marker="o", alpha=0.5, linewidth=1.0)
+
+                # Calo hit
+                elif input_name[1] == "c":
+                    ax[ax_idx].scatter(x[mask], y[mask], color=color, marker="s", alpha=0.2)
+                
+                # Muon hit
+                elif input_name == "muon":
+                    ax[ax_idx].scatter(x[mask], y[mask], color=color, marker="+", alpha=0.2)
+                
+                ax[ax_idx].set_xlabel(ax_spec["x"])
+                ax[ax_idx].set_ylabel(ax_spec["y"])
+                ax[ax_idx].set_aspect("equal", "box")
+    
+    return fig
