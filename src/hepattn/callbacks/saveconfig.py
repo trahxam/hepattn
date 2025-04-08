16b7147683dedd504daacb63e5d1da5055c1e9a2
svanstro <sam.van.stroud@cern.ch>
2025-04-08 10:29:02 +0100

    log hyperparams to comet

--- src/hepattn/callbacks/saveconfig.py
+++ src/hepattn/callbacks/saveconfig.py
@@ -18,7 +18,7 @@ class Metadata(Callback):
             print(f"log dir:\n{trainer.log_dir}")
             print("-" * 80)
 
-    def on_fit_start(self, trainer: Trainer, pl_module: LightningModule) -> None:  # noqa: ARG002
+    def on_fit_start(self, trainer: Trainer, pl_module: LightningModule) -> None:
         self.trainer = trainer
         if not trainer.is_global_zero:
             return
@@ -26,12 +26,12 @@ class Metadata(Callback):
             return
 
         log_dir = Path(trainer.log_dir)
-        self.save_metadata(log_dir)
+        self.save_metadata(log_dir, pl_module)
         if isinstance(self.trainer.logger, CometLogger):
             for file in log_dir.glob("*.yaml"):
                 self.trainer.logger.experiment.log_asset(file)
 
-    def save_metadata(self, log_dir: Path) -> None:
+    def save_metadata(self, log_dir: Path, pl_module: LightningModule) -> None:
         trainer = self.trainer
         logger = trainer.logger
         datamodule = trainer.datamodule
@@ -53,6 +53,9 @@ class Metadata(Callback):
         }
         if hasattr(trainer, "timestamp"):
             meta["timestamp"] = trainer.timestamp
+
+        if logger := pl_module.logger:
+            logger.log_hyperparams(meta)
 
         meta_path = log_dir / "metadata.yaml"
         with meta_path.open("w") as file:
