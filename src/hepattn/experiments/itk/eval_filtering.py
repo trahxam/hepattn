252c04cc8312802cc3190e3b90865f6add77f8cc
Max Hart <maxhart@hypatia-login.local>
2025-04-29 15:56:10 +0100

    refactor itk eval code

--- src/hepattn/experiments/itk/eval_filtering.py
+++ src/hepattn/experiments/itk/eval_filtering.py
@@ -3,52 +3,55 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 import numpy as np
 import yaml
+import h5py
 from scipy.stats import binned_statistic
 from tqdm import tqdm
 
-from hepattn.experiments.itk.data import ITkDataModule
+from hepattn.experiments.itk.data import ITkDataset
+from hepattn.utils.eval_plots import plot_hist_to_ax, bayesian_binomial_error
+
 
 plt.rcParams["figure.dpi"] = 300
 plt.rcParams["font.size"] = 8
 plt.rcParams["figure.constrained_layout.use"] = True
 
 
-def plot_hist_to_ax(ax, values, bins, value_errors=None, color="cornflowerblue"):
-    for bin_idx in range(len(bins) - 1):
-        px = np.array([bins[bin_idx], bins[bin_idx + 1]])
-        py = np.array([values[bin_idx], values[bin_idx]])
-
-        ax.plot(px, py, color=color, linewidth=1.0)
-
-        if value_errors is not None:
-            pe = np.array([value_errors[bin_idx], value_errors[bin_idx]])
-            ax.fill_between(px, py - pe, py + pe, color=color, alpha=0.1, ec="none")
-
-
-def frequentist_binomial_error(k, n):
-    return np.sqrt((k / n) * (1 - (k / n)) / n)
-
-
-def bayesian_binomial_error(k, n):
-    return np.sqrt(((k + 1) * (k + 2)) / ((n + 2) * (n + 3)) - ((k + 1) / (n + 2)) ** 2)
+def sigmoid(x):
+    return 1/(1 + np.exp(-np.clip(x, -10, 10)))
 
 
 def main():
+    # Arguments for the evaluation
+    # First specify the config which will provide things like input info etc
     config_path = Path("/share/rcifdata/maxhart/hepattn-test/hepattn/src/hepattn/experiments/itk/configs/filtering_pixel.yaml")
+    recon_max_eta = 4.0
+    recon_min_pt = 1.0
+    recon_min_num_pixel = 3.0
+
+    # Now create the dataset
     config = yaml.safe_load(config_path.read_text())["data"]
-    config["num_workers"] = 0
+    inputs = config["inputs"]
 
-    datamodule = ITkDataModule(**config)
-    datamodule.setup(stage="test")
+    # Add in extra target fields that will allow us to recompute reconstructability
+    targets = config["targets"]
+    targets["particle"] = ["pt", "eta", "phi"]
+    targets["particle_pixel"] = []
 
-    dataloader = datamodule.test_dataloader()
-    dataset = dataloader.dataset
+    dataset = ITkDataset(
+        dirpath=config["test_dir"],
+        inputs=inputs,
+        targets=targets,
+        num_events=-1,
+        hit_regions=config["hit_regions"],
+        particle_min_pt=recon_min_pt,
+        particle_max_abs_eta=recon_max_eta,
+        particle_min_num_hits={"pixel": recon_min_num_pixel},
+        event_max_num_particles=5000,
+    )
 
-    dataset.append_hit_eval_output = True
-    dataset.inputs["pixel"].append("filter_logit")
-    dataset.targets["particle"] = ["pt", "eta", "phi"]
-    dataset.targets["particle_pixel"] = []
-
+    # Give the test eval file we are evaluating and setup the file
+    hit_eval_path = "/share/rcifdata/maxhart/hepattn-test/hepattn/logs/ITk_pixel_region135_eta4_900mev_20250428-T091926/ckpts/epoch=061-val_loss=0.43928_test_eval.h5"
+    
     # Define bins for particle retention rate under the nominal working point
     particle_bins = {"pt": np.linspace(0.5, 10.0, 32), "eta": np.linspace(-4, 4, 32), "phi": np.linspace(-np.pi, np.pi, 32)}
     particle_pre_counts = {field: np.zeros(len(particle_bins[field]) - 1) for field in particle_bins}
@@ -57,32 +60,41 @@ def main():
     num_hits_pre = []
     num_recon_parts_pre = []
 
-    working_points = [0.01, 0.05, 0.075, 0.1, 0.125]
+    working_points = [0.01, 0.05, 0.1]
     wp_num_hits_post = {wp: [] for wp in working_points}
     wp_num_recon_parts_post = {wp: [] for wp in working_points}
 
+    hit = "pixel"
+    
+    # Working point that is used for the bulk plots
+    nominal_wp = 0.05
+
     # Iterate over the events
-    for i in tqdm(range(10)):
+    for idx in tqdm(range(100)):
         # Load the data from the event
-        inputs, targets = dataset[i]
+        sample_id = dataset.sample_ids[idx]
+        
+        # Note we are using load event, so evenerying is numpy arrays that are unbatched
+        inputs, targets = dataset.load_event(sample_id)
+
+        with h5py.File(hit_eval_path, "r") as hit_eval_file:
+            hit_logits = hit_eval_file[f"{sample_id}/outputs/final/{hit}_filter/{hit}_logit"][0]
 
         # Particles which are deemed reconstructable pre-filter
-        particle_recon_pre = targets["particle_valid"][0]
-        particle_hit_valid_pre = targets["particle_pixel_valid"][0]
+        particle_recon_pre = targets["particle_valid"]
+        particle_hit_valid_pre = targets["particle_pixel_valid"]
 
-        # Drop any padding
+        # Drop any invalid particles that are not reconstructable
+        # The pt and eta cuts should be applied by the dataloader
         particle_hit_valid_pre = particle_hit_valid_pre[particle_recon_pre]
         particle_recon_pre = particle_recon_pre[particle_recon_pre]
-
-        # Get the hit filter logits
-        hit_logits = inputs["pixel_filter_logit"][0]
 
         # Record number of reconstructable particles and hits before filtering
         num_hits_pre.append(particle_hit_valid_pre.shape[-1])
         num_recon_parts_pre.append(particle_recon_pre.sum())
 
         # Mark hits which pass the filter
-        hit_filter_pred = hit_logits.sigmoid() >= 0.05
+        hit_filter_pred = sigmoid(hit_logits) >= nominal_wp
 
         # The post filter mask is just the pre filter mask, but with filtered hits removed
         particle_hit_valid_post = particle_hit_valid_pre & hit_filter_pred[None, :]
@@ -90,10 +102,10 @@ def main():
 
         # Fill the particle histograms
         for field, bins in particle_bins.items():
-            particle_field = targets[f"particle_{field}"][0][targets["particle_valid"][0]]
+            particle_field = targets[f"particle_{field}"][targets["particle_valid"]]
 
-            pre_count, _, _ = binned_statistic(particle_field, particle_recon_post.float(), statistic="sum", bins=bins)
-            post_count, _, _ = binned_statistic(particle_field, particle_recon_post.float(), statistic="count", bins=bins)
+            post_count, _, _ = binned_statistic(particle_field, particle_recon_post, statistic="sum", bins=bins)
+            pre_count, _, _ = binned_statistic(particle_field, particle_recon_pre, statistic="sum", bins=bins)
 
             particle_pre_counts[field] += pre_count
             particle_post_counts[field] += post_count
@@ -101,7 +113,7 @@ def main():
         # Now calculate metrics for different working points
         for working_point in working_points:
             # Mark hits which pass the filter under this new working point
-            hit_filter_pred = inputs["pixel_filter_logit"][0].sigmoid() >= working_point
+            hit_filter_pred = sigmoid(hit_logits) >= working_point
             particle_hit_valid_post = particle_hit_valid_pre & hit_filter_pred[None, :]
             particle_recon_post = particle_hit_valid_post.sum(-1) >= 3
 
