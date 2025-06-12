09c419482fc4aa4aa230502b5792d17420226c13
Max Hart <maxhart@hypatia-login.local>
2025-06-12 08:35:46 +0100

    add extra plot options

--- src/hepattn/utils/fix_yaml.py
+++ src/hepattn/utils/fix_yaml.py
@@ -8,6 +8,6 @@ yaml.indent(mapping=2, sequence=4, offset=2)
 for path in Path().rglob("*.yml"):
     with Path(path).open(mode="r") as f:
         data = yaml.load(f)
-        
+
     with Path(path).open(mode="w") as f:
         yaml.dump(data, f)
