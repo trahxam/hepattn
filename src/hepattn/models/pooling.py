336f3de10acb196c227c032400f1c52f13bc0696
Max Hart <maxhart@hypatia-login.local>
2025-06-16 09:57:44 +0100

    clean up pooling

--- src/hepattn/models/pooling.py
+++ src/hepattn/models/pooling.py
@@ -41,10 +41,11 @@ class Pooling(nn.Module):
         if self.pool_net is not None:
             x = self.pool_net(x) # (..., N, E) -> (..., N, E)
         # Calculate a weight that will be used to pool the new embeddings (..., N, E) -> (..., N, 1)
-        w = self.weight_net(x)
+        w = self.weight_net(x).squeeze(-1) # (..., N)
         # Set weights of padded entries to zero and make sure they sum to one
         w = w.masked_fill(~inputs[self.input_object  + "_valid"], -torch.inf)
-        w = torch.softmax(w, dim=-2)  # (..., N, 1)
+        w = torch.softmax(w, dim=-1)  # (..., N)
+        w = w.masked_fill(~inputs[self.input_object  + "_valid"], 0.0)
         # Weighted sum of all the embeddings (..., N, E) -> (..., E)
-        x = torch.sum(x * w, dim=-2)
+        x = torch.sum(x * w.unsqueeze(-1), dim=-2)
         return {self.output_object + "_embed": x}
