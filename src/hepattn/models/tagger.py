8bf0ab8f59e6abf1a284b4caee529a9ec8442ea5
Max Hart <maxhart@hypatia-login.local>
2025-06-16 12:48:38 +0100

    add roi tagging

---
+++ src/hepattn/models/tagger.py
@@ -0,0 +1,104 @@
+import torch
+from torch import Tensor, nn
+
+
+class Tagger(nn.Module):
+    def __init__(
+        self,
+        input_nets: nn.ModuleList,
+        encoder: nn.Module,
+        pooling: nn.Module,
+        tasks: nn.ModuleList,
+        dim: int,
+    ):
+        super().__init__()
+
+        self.input_nets = input_nets
+        self.encoder = encoder
+        self.pooling = pooling
+        self.tasks = tasks
+        self.dim = dim
+
+    def forward(self, inputs: dict[str, Tensor]) -> dict[str, Tensor]:
+        # Atomic input names
+        input_names = [input_net.input_name for input_net in self.input_nets]
+
+        assert "key" not in input_names, "'key' input name is reserved."
+        assert "query" not in input_names, "'query' input name is reserved."
+
+        x = {}
+
+        # Embed the input objects
+        for input_net in self.input_nets:
+            input_name = input_net.input_name
+            x[input_name + "_embed"] = input_net(inputs)
+            x[input_name + "_valid"] = inputs[input_name + "_valid"]
+
+            # These slices can be used to pick out specific
+            # objects after we have merged them all together
+            device = inputs[input_name + "_valid"].device
+            x[f"key_is_{input_name}"] = torch.cat(
+                [torch.full((inputs[i + "_valid"].shape[-1],), i == input_name, device=device, dtype=torch.bool) for i in input_names], dim=-1
+            )
+
+        # Merge the input objects and he padding mask into a single set
+        x["key_embed"] = torch.concatenate([x[input_name + "_embed"] for input_name in input_names], dim=-2)
+        x["key_valid"] = torch.concatenate([x[input_name + "_valid"] for input_name in input_names], dim=-1)
+
+        # Pass merged input hits through the encoder
+        x["key_embed"] = self.encoder(x["key_embed"], kv_mask=x.get("key_valid"))
+
+        # Unmerge the updated features back into the separate input types
+        # These are just views into the tensor that old all the merged hits
+        for input_name in input_names:
+            x[input_name + "_embed"] = x["key_embed"][..., x[f"key_is_{input_name}"], :]
+
+        # Compute and add the pooled embeddings to out embedding set
+        x = x | self.pooling(x)
+
+        # Get the final outputs
+        outputs = {"final": {}}
+        for task in self.tasks:
+            outputs["final"][task.name] = task(x)
+
+        return outputs
+
+    def predict(self, outputs: dict) -> dict:
+        """Takes the raw model outputs and produces a set of actual inferences / predictions.
+        For example will take output probabilies and apply threshold cuts to prduce boolean predictions.
+
+        Parameters
+        ----------
+        predictions:
+            The predictions used for inference from the model.
+        """
+        preds = {}
+
+        # Compute predictions for each task in each block
+        for layer_name, layer_outputs in outputs.items():
+            preds[layer_name] = {}
+
+            for task in self.tasks:
+                preds[layer_name][task.name] = task.predict(layer_outputs[task.name])
+
+        return preds
+
+    def loss(self, outputs: dict, targets: dict) -> dict:
+        """Computes the loss between the forward pass of the model and the data / targets.
+
+        Parameters
+        ----------
+        outputs:
+            The outputs from the forward pass of the model.
+        targets:
+            The data containing the targets.
+        """
+
+        # Compute the losses for each task in each block
+        losses = {}
+        for layer_name in outputs:
+            losses[layer_name] = {}
+            for task in self.tasks:
+                losses[layer_name][task.name] = task.loss(outputs[layer_name][task.name], targets)
+
+        return losses
