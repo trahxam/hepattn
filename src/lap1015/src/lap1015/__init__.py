4b389db3ce9682cdc5fe9deb0cd6edf969920c99
svanstro <sam.van.stroud@cern.ch>
2025-06-18 13:36:49 +0100

    remove explicit omp

--- src/lap1015/src/lap1015/__init__.py
+++ src/lap1015/src/lap1015/__init__.py
@@ -1,12 +1,10 @@
 from __future__ import annotations
-import os
+
 from functools import partial
 
 from ._core import linear_sum_assignment
 
-os.environ["OMP_NUM_THREADS"] = "2"
-
 lap_late = partial(linear_sum_assignment, omp=False, eps=False)
 lap_early = partial(linear_sum_assignment, omp=True, eps=True)
 
-__all__ = ["linear_sum_assignment", "lap_late", "lap_early"]
+__all__ = ["lap_early", "lap_late", "linear_sum_assignment"]
