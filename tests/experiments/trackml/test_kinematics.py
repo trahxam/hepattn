de36f387bbf2ce32fe88fb7c0d7fedff4f955fde
Sebastien Rettie <sebrettie@gmail.com>
2025-06-18 10:26:00 +0200

    add mkdir

--- tests/experiments/trackml/test_kinematics.py
+++ tests/experiments/trackml/test_kinematics.py
@@ -48,4 +48,6 @@ class TestTrackMLEvent:
 
     def test_trackml_plot_kinematics(self, trackml_dataset):
         fig = plot_trackml_kinematics(trackml_dataset)
+        output_dir = Path("tests/outputs/trackml/")
+        output_dir.mkdir(exist_ok=True, parents=True)
         fig.savefig(Path("tests/outputs/trackml/trackml_kinematics.png"))
