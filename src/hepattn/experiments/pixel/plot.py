04edf5177066a0e8b69fdb8f6c5299d779f96ada
Max Hart <maxhart@compute-gpu-0-0.local>
2025-06-27 09:06:17 +0100

    update pixel plotting

--- src/hepattn/experiments/pixel/plot.py
+++ src/hepattn/experiments/pixel/plot.py
@@ -1,10 +1,14 @@
-from pathlib import Path
-
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
 import yaml
+import matplotlib.cm as cm
+
 from matplotlib import colors
+from matplotlib.patches import Rectangle
+from pathlib import Path
+from scipy.stats import binned_statistic
+
 
 from hepattn.experiments.pixel.data import PixelClusterDataModule
 
@@ -23,47 +27,101 @@ dataloader = datamodule.test_dataloader()
 
 inputs, targets = next(iter(dataloader))
 
+
+print("Average query occupancy:", targets["particle_valid"].float().mean())
+
 particle_masks = {
     "All": targets["particle_valid"],
-    "Primary": targets["particle_primary"].to(torch.bool),
-    "Secondary": targets["particle_secondary"].to(torch.bool),
-    "No Truth": targets["particle_notruth"].to(torch.bool),
-    "Primary or Secondary": targets["particle_primary"].to(torch.bool) | targets["particle_secondary"].to(torch.bool),
+    "Primary": targets["particle_primary"].to(torch.bool) & targets["particle_valid"],
+    "Secondary": targets["particle_secondary"].to(torch.bool) & targets["particle_valid"],
+    "No Truth": targets["particle_notruth"].to(torch.bool) & targets["particle_valid"],
+    "Primary or Secondary": (targets["particle_primary"].to(torch.bool) | targets["particle_secondary"].to(torch.bool)) & targets["particle_valid"],
 }
+
+multiplicities, multiplicity_counts = np.unique(targets["cluster_multiplicity"], return_counts=True)
+
+for multiplicity, multiplicity_count in zip(multiplicities, multiplicity_counts):
+    print(f"{str(int(multiplicity)).ljust(4)} | {str(int(multiplicity_count)).ljust(8)} | {(100 * multiplicity_count / len(targets["cluster_multiplicity"])):.2f}%")
 
 # Plot the particle counts
 fig, ax = plt.subplots()
 fig.set_size_inches(8, 2)
 
 for mask_name, particle_mask in particle_masks.items():
+
+    pct = 100 * particle_mask.sum() / targets["particle_valid"].sum()
+    print(f"{mask_name.ljust(24)}| {str(particle_mask.sum().item()).ljust(6)}/{str(targets["particle_valid"].sum().item()).ljust(6)} | {pct:.2f}%")
+
     cluster_num_particles = particle_mask.sum(-1)
     ax.hist(
         cluster_num_particles,
         bins=np.arange(0, 16) - 0.5,
         label=mask_name,
         histtype="step",
+        density=False,
     )
 
-ax.legend(fontsize=10)
+ax.legend(fontsize=6)
 ax.set_yscale("log")
 ax.set_xticks(np.arange(0, 16))
-ax.grid(zorder=0, alpha=0.25, linestyle="--")
-ax.set_ylabel("Count")
+ax.grid(alpha=0.25, linestyle="--")
+ax.set_ylabel("Density")
 ax.set_xlabel("Number of Pixels on Cluster")
 
 fig.tight_layout()
 fig.savefig("src/hepattn/experiments/pixel/plots/cluster_num_particles_hist.png")
 
+fig, ax = plt.subplots(nrows=1, ncols=2)
+fig.set_size_inches(8, 2)
+
+ax[0].hist(inputs["cluster_width_x"], bins=32, histtype="step", color="cornflowerblue")
+ax[1].hist(inputs["cluster_width_y"], bins=32, histtype="step", color="cornflowerblue")
+
+ax[0].grid(alpha=0.25, linestyle="--")
+ax[1].grid(alpha=0.25, linestyle="--")
+
+ax[0].set_xlabel(r"Cluster Width $x$")
+ax[1].set_xlabel(r"Cluster Width $y$")
+
+ax[0].set_ylabel("Count")
+ax[1].set_ylabel("Count")
+
+ax[0].set_yscale("log")
+ax[1].set_yscale("log")
+
+fig.tight_layout()
+fig.savefig("src/hepattn/experiments/pixel/plots/cluster_width.png")
+
+
+fields = ["global_r", "global_eta", "global_phi"]
+field_aliases = {
+    "global_r": r"Cluster Global $r$",
+    "global_eta": r"Cluster Global $\eta$",
+    "global_phi": r"Cluster Global $\phi$",
+}
+
+fig, ax = plt.subplots(nrows=1, ncols=len(fields))
+fig.set_size_inches(8, 2)
+
+for i, field in enumerate(fields):
+    ax[i].hist(inputs[f"cluster_{field}"], bins=32, histtype="step", color="cornflowerblue")
+    ax[i].set_xlabel(field_aliases[field])
+    ax[i].set_ylabel("Count")
+    ax[i].grid(alpha=0.25, linestyle="--")
+
+fig.tight_layout()
+fig.savefig("src/hepattn/experiments/pixel/plots/cluster_global_coords.png")
+
 # Plot the particle fields
 
-fields = ["x", "y", "theta", "phi", "p"]
+fields = ["x", "y"]
 
 field_bins = {
     "x": np.linspace(-8, 8, 24),
     "y": np.linspace(-4, 4, 24),
     "theta": np.linspace(-np.pi, np.pi, 24),
-    "phi": np.linspace(-np.pi, np.pi, 24),
-    "p": np.logspace(-1, 3, 24),
+    "phi": np.linspace(-np.pi/4, np.pi/4, 24),
+    "p": np.logspace(-1, 4, 24),
 }
 
 field_symbols = {
@@ -76,6 +134,8 @@ field_symbols = {
 
 log_fields = ["p"]
 
+fields = ["x", "y"]
+
 fig, ax = plt.subplots(nrows=1, ncols=len(fields))
 fig.set_size_inches(8, 2)
 
@@ -86,6 +146,7 @@ for i, field in enumerate(fields):
             bins=field_bins[field],
             label=mask_name,
             histtype="step",
+            density=True,
         )
 
         if field in log_fields:
@@ -93,20 +154,51 @@ for i, field in enumerate(fields):
 
     ax[i].set_xlabel("Particle " + field_symbols[field])
     ax[i].set_yscale("log")
-    ax[i].grid(zorder=0, alpha=0.25, linestyle="--")
+    ax[i].grid(alpha=0.25, linestyle="--")
 
-ax[0].set_ylabel("Count")
+ax[0].set_ylabel("Density")
+ax[-1].legend(fontsize=6)
 
 fig.tight_layout()
-fig.savefig("src/hepattn/experiments/pixel/plots/cluster_particle_fields_hist.png")
+fig.savefig("src/hepattn/experiments/pixel/plots/cluster_particle_xy_hist.png")
 
-# Plot the cell charge
+fields = ["theta", "phi", "p"]
+
+fig, ax = plt.subplots(nrows=1, ncols=len(fields))
+fig.set_size_inches(8, 2)
+
+for i, field in enumerate(fields):
+    for mask_name, particle_mask in particle_masks.items():
+        ax[i].hist(
+            targets[f"particle_{field}"][particle_mask.to(torch.bool)],
+            bins=field_bins[field],
+            label=mask_name,
+            histtype="step",
+            density=True,
+        )
+
+        if field in log_fields:
+            ax[i].set_xscale("log")
+
+    ax[i].tick_params(axis="x", which="both", bottom=False, top=False, labelbottom=False)
+    ax[i].tick_params(axis="y", which="both", bottom=False, top=False, labelbottom=False)
+    ax[i].set_xlabel("Particle " + field_symbols[field])
+    ax[i].set_yscale("log")
+    ax[i].grid(True, alpha=0.25, linestyle="--")
+
+ax[0].set_ylabel("Density")
+ax[-1].legend(fontsize=6)
+
+fig.tight_layout()
+fig.savefig("src/hepattn/experiments/pixel/plots/cluster_particle_angles_hist.png")
+
+# Plot the pixel charge
 
 fig, ax = plt.subplots()
 fig.set_size_inches(8, 2)
 
 ax.hist(
-    inputs["cell_charge"][inputs["cell_valid"]],
+    inputs["pixel_charge"][inputs["pixel_valid"]],
     bins=np.logspace(-2, 0, 64),
     histtype="step",
     color="cornflowerblue",
@@ -115,41 +207,46 @@ ax.hist(
 ax.set_yscale("log")
 ax.set_xscale("log")
 
-ax.set_ylabel("Count")
+ax.set_ylabel("Density")
 ax.set_xlabel("Pixel Charge [ke / 100]")
+ax.grid(alpha=0.25, linestyle="--")
 
 fig.tight_layout()
-fig.savefig("src/hepattn/experiments/pixel/plots/charge.png")
+fig.savefig("src/hepattn/experiments/pixel/plots/pixel_charge.png")
 
-# Plot the cell coordinates
+# Plot the pixel coordinates
 
 fig, ax = plt.subplots(nrows=1, ncols=2)
 fig.set_size_inches(8, 2)
 
 ax[0].hist(
-    inputs["cell_x"][inputs["cell_valid"]],
+    inputs["pixel_x"][inputs["pixel_valid"]],
     bins=np.linspace(-5, 5, 11),
     histtype="step",
     color="cornflowerblue",
+    density=True,
 )
 
 ax[1].hist(
-    inputs["cell_y"][inputs["cell_valid"]],
+    inputs["pixel_y"][inputs["pixel_valid"]],
     bins=np.linspace(-5, 5, 11),
     histtype="step",
     color="cornflowerblue",
+    density=True,
 )
 
 ax[0].set_xlabel(r"Pixel $x$ Index Position")
-ax[0].set_ylabel("Count")
+ax[0].set_ylabel("Density")
 ax[0].set_yscale("log")
+ax[0].grid(alpha=0.25, linestyle="--")
 
 ax[1].set_xlabel(r"Pixel $y$ Index Position")
-ax[1].set_ylabel("Count")
+ax[1].set_ylabel("Density")
 ax[1].set_yscale("log")
+ax[1].grid(alpha=0.25, linestyle="--")
 
 fig.tight_layout()
-fig.savefig("src/hepattn/experiments/pixel/plots/cell_local_xy.png")
+fig.savefig("src/hepattn/experiments/pixel/plots/pixel_local_xy.png")
 
 # Plot some examples
 
@@ -159,42 +256,39 @@ fig.set_size_inches(8, 8)
 ax = ax.flatten()
 
 idx = torch.argsort(targets["particle_valid"].sum(-1), descending=True)
+idx = torch.argsort(inputs["pixel_valid"].sum(-1), descending=True)
 
-cell_charge = inputs["cell_charge"][inputs["cell_valid"]]
-norm = colors.Normalize(vmin=torch.min(cell_charge), vmax=torch.min(cell_charge))
+pixel_charge = inputs["pixel_charge"][inputs["pixel_valid"]]
+norm = colors.LogNorm(vmin=0.01, vmax=1.0)
+cmap = plt.get_cmap("viridis")
 
-for ax_idx in range(len(ax)):
+for ax_idx in range(len(ax) - 1):
     i = idx[ax_idx]
 
-    extent = 4.5
+    lx = torch.floor(torch.min(inputs["pixel_x"][i])) - 1.5
+    ux = torch.floor(torch.max(inputs["pixel_x"][i])) + 1.5
+    ly = torch.floor(torch.min(inputs["pixel_y"][i])) - 1.5
+    uy = torch.floor(torch.max(inputs["pixel_y"][i])) + 1.5
 
-    ax[ax_idx].set_xticks(np.linspace(-extent, extent, int(2 * extent + 1)))
-    ax[ax_idx].set_yticks(np.linspace(-extent, extent, int(2 * extent + 1)))
+    ax[ax_idx].set_xticks(np.linspace(lx, ux, int(abs(lx) + abs(ux) + 1)))
+    ax[ax_idx].set_yticks(np.linspace(ly, uy, int(abs(ly) + abs(uy) + 1)))
 
-    ax[ax_idx].grid(alpha=0.5, zorder=-100)
-    ax[ax_idx].grid(alpha=0.5, zorder=-100)
-
-    # ax[ax_idx].spines['left'].set_visible(False)
-    # ax[ax_idx].spines['right'].set_visible(False)
-    # ax[ax_idx].spines['top'].set_visible(False)
-    # ax[ax_idx].spines['bottom'].set_visible(False)
+    ax[ax_idx].grid(alpha=0.5)
+    ax[ax_idx].grid(alpha=0.5)
 
     ax[ax_idx].set_xticklabels([])
     ax[ax_idx].set_yticklabels([])
 
-    ax[ax_idx].imshow(
-        np.zeros(shape=(10, 10)) * np.nan,
-        extent=(-extent, extent, -extent, extent),
-    )
+    ax[ax_idx].imshow(np.zeros(shape=(10, 10)) * np.nan, extent=(lx, ux, ly, uy),)
 
-    ax[ax_idx].scatter(
-        inputs["cell_x"][i][inputs["cell_valid"][i]],
-        inputs["cell_y"][i][inputs["cell_valid"][i]],
-        c=inputs["cell_charge"][i][inputs["cell_valid"][i]],
-        marker="s",
-        ec="black",
-        s=64,
-    )
+    for j in range(len(inputs["pixel_valid"][i])):
+        size = 0.8
+        x = inputs["pixel_x"][i][j]
+        y = inputs["pixel_y"][i][j]
+        charge = inputs["pixel_charge"][i][j]
+        pixel_patch = Rectangle((x - size/2, y - size/2), size, size, color=cmap(norm(charge)))
+        ax[ax_idx].add_patch(pixel_patch)
+
 
     particles_x = targets["particle_x"][i][targets["particle_valid"][i]]
     particles_y = targets["particle_y"][i][targets["particle_valid"][i]]
@@ -205,19 +299,33 @@ for ax_idx in range(len(ax)):
     particles_dx = particles_phi / torch.sqrt(particles_phi**2 + particles_theta**2)
     particles_dy = particles_theta / torch.sqrt(particles_phi**2 + particles_theta**2)
 
-    ax[ax_idx].scatter(particles_x, particles_y, c="cornflowerblue", ec="black", s=32, linewidths=1)
-
     num_particles = targets["particle_valid"][i].sum(-1)
 
-    for particle_idx in range(num_particles):
+    for j in range(num_particles):
+        if targets["particle_primary"][i][j]:
+            c = "cornflowerblue"
+        elif targets["particle_secondary"][i][j]:
+            c = "crimson"
+        elif targets["particle_notruth"][i][j]:
+            c = "darkgray"
+
+        ax[ax_idx].scatter(particles_x[j], particles_y[j], c=c, ec="black", s=32, linewidths=1)
+
         ax[ax_idx].arrow(
-            particles_x[particle_idx],
-            particles_y[particle_idx],
-            particles_dx[particle_idx],
-            particles_dy[particle_idx],
+            particles_x[j],
+            particles_y[j],
+            particles_dx[j],
+            particles_dy[j],
             width=0.01,
         )
 
+ax[-1].axis("off")
+
+sm = cm.ScalarMappable(norm=norm, cmap=cmap)
+sm.set_array([])
+
+colorbar = fig.colorbar(sm, ax=ax[-1], location="bottom", aspect=5.0, panchor=(0.5, 0.5))
+colorbar.set_label("Pixel Charge [ke / 100]", fontsize=8)
 
 fig.tight_layout()
 fig.savefig("src/hepattn/experiments/pixel/plots/examples.png")
