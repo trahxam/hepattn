c85cf3505b5d6b48b099d4ef04fe8d785f9f2465
Max Hart <maxhart@hypatia-login.local>
2025-08-06 13:28:59 +0100

    fix encoder bug where kv mask is not permuted

--- src/hepattn/models/transformer.py
+++ src/hepattn/models/transformer.py
@@ -232,7 +232,11 @@ class Encoder(nn.Module):
         x_sort_idx = None
         if x_sort_value is not None:
             x_sort_idx = torch.argsort(x_sort_value, axis=-1)
-            x = torch.gather(x, -2, x_sort_idx.unsqueeze(-1).expand_as(x))
+            x = torch.gather(x, dim=-2, index=x_sort_idx.unsqueeze(-1).expand_as(x))
+
+            # Also permute the kv mask if we have one
+            if kwargs.get("kv_mask") is not None:
+                kwargs["kv_mask"] = torch.gather(kwargs["kv_mask"], dim=1, index=x_sort_idx)
 
         # Add register tokens at the beginning of the sequence
         if self.register_tokens is not None:
