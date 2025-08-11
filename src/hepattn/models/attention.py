fa14e691e6eaec537cf7289c28e5a95c198a37e6
Sam VS <sam.van.stroud@cern.ch>
2025-08-11 11:15:02 +0100

    prepare for isambard merge (#106)
    * small things
    
    * flash attention stuff
    
    * minor

--- src/hepattn/models/attention.py
+++ src/hepattn/models/attention.py
@@ -1,12 +1,17 @@
 import torch
 import torch.nn.functional as F
-from flash_attn import flash_attn_func, flash_attn_varlen_func
-from flash_attn.bert_padding import pad_input, unpad_input
+
+try:
+    from flash_attn_interface import flash_attn_func, flash_attn_varlen_func  # FA3 (from source)
+except ImportError:
+    from flash_attn import flash_attn_func, flash_attn_varlen_func
+
 from torch import BoolTensor, Size, Tensor, nn
 from torch.nn.attention.flex_attention import BlockMask, _score_mod_signature, flex_attention
 from torch.nn.functional import scaled_dot_product_attention
 
 from hepattn.models.norm import LayerNorm
+from hepattn.utils.bert_padding import pad_input, unpad_input
 
 ATTN_TYPES = {
     "torch": scaled_dot_product_attention,
