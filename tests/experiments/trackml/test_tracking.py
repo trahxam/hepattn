be006c9ec2b2948a7b131607141ad1772101c6c9
Sam VS <sam.van.stroud@cern.ch>
2025-08-19 12:41:44 +0100

    another fix (#129)

--- tests/experiments/trackml/test_tracking.py
+++ tests/experiments/trackml/test_tracking.py
@@ -4,3 +4,8 @@ from hepattn.experiments.trackml import run_tracking
 def test_run_tracking():
     args = ["fit", "--config", "tests/experiments/trackml/test_tracking.yaml"]
     run_tracking.main(args)
+
+
+def test_run_tracking_old_sort():
+    args = ["fit", "--config", "tests/experiments/trackml/test_tracking_old_sort.yaml"]
+    run_tracking.main(args)
