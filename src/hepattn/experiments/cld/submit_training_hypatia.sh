68404e81c51a7f6581bcbf1de878ed4b56fe0429
Max Hart <maxhart@compute-gpu-0-2.local>
2025-10-27 12:01:46 +0000

    add slurm cuda bug workaround

--- src/hepattn/experiments/cld/submit_training_hypatia.sh
+++ src/hepattn/experiments/cld/submit_training_hypatia.sh
@@ -33,14 +33,44 @@ echo "Moved dir, now in: ${PWD}"
 # Set tmpdir
 export TMPDIR=/share/rcifdata/maxhart/tmp/
 
+# get list of valid gpu indices from nvidia-smi, e.g. "0 1 2 3"
+VALID_GPU_IDS=($(nvidia-smi --query-gpu=index --format=csv,noheader))
+
+# current assignment from slurm
+REQ_GPU_ID="$CUDA_VISIBLE_DEVICES"
+
+is_valid=false
+for gid in "${VALID_GPU_IDS[@]}"; do
+    if [ "$gid" = "$REQ_GPU_ID" ]; then
+        is_valid=true
+        break
+    fi
+done
+
+if [ "$is_valid" = true ]; then
+    echo "CUDA_VISIBLE_DEVICES is valid: $CUDA_VISIBLE_DEVICES"
+else
+    echo "CUDA_VISIBLE_DEVICES ($CUDA_VISIBLE_DEVICES) is invalid. Reassigning."
+
+    # pick gpu with lowest memory.used
+    DEV=$(nvidia-smi --query-gpu=index,memory.used --format=csv,noheader \
+        | sort -t, -k2 -n \
+        | head -n1 \
+        | cut -d',' -f1 \
+        | xargs)
+
+    export CUDA_VISIBLE_DEVICES="$DEV"
+    echo "Using GPU: $DEV (CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES)"
+fi
+
 # Run the training
 echo "Running training script..."
 
 # Python command that will be run
-#CONFIG_PATH="/share/rcifdata/maxhart/hepattn/logs/CLD_TRKECALHCAL_16_96_TF_charged_10MeV_F16_scaled_20250526-T120023/config.yaml"
-#CKPT_PATH="/share/rcifdata/maxhart/hepattn/logs/CLD_TRKECALHCAL_16_96_TF_charged_10MeV_F16_scaled_20250526-T120023/ckpts/epoch=009-train_loss=1.45212.ckpt"
+#CONFIG_PATH="/share/rcifdata/maxhart/hepattn/logs/CLD_2_320_10MeV_neutrals_20251026-T230553/config.yaml"
+#CKPT_PATH="/share/rcifdata/maxhart/hepattn/logs/CLD_2_320_10MeV_neutrals_20251026-T230553/ckpts/epoch=000-train_loss=3.05229.ckpt"
 #PYTORCH_CMD="python src/hepattn/experiments/cld/main.py fit --config $CONFIG_PATH --ckpt_path $CKPT_PATH"
-PYTORCH_CMD="python src/hepattn/experiments/cld/main.py fit --config src/hepattn/experiments/cld/configs/base.yaml"
+PYTORCH_CMD="python src/hepattn/experiments/cld/main.py fit --config src/hepattn/experiments/cld/configs/base.yaml --config src/hepattn/experiments/cld/configs/harmonics.yaml"
 
 # Pixi commnand that runs the python command inside the pixi env
 PIXI_CMD="pixi run $PYTORCH_CMD"
